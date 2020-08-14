ae8b9a30e8d0647a3f129c647155f4fdf5005d5e
Marat <remarr@gmail.com>
2020-08-14 23:27:48 +0300

    clean up BroadMessage

--- burevestnik/BroadMessage.swift
+++ burevestnik/BroadMessage.swift
@@ -12,25 +12,9 @@ struct BroadMessage: Equatable {
   let ti: Date
   let msg: String
 
-  var discoveryInfo: [String: String] {
-    let res = [
-      ti.timeIntervalSinceReferenceDate.description: msg,
-    ]
-    assert(res.description.count <= 400)
-    return res
-  }
-
   init(_ msg: String, _ ti: Date = Date()) {
     self.ti = ti
     self.msg = msg
-  }
-
-  static func from(dic: [String: String]) -> [BroadMessage] {
-    dic.map { ti, msg in
-      let date = TimeInterval(ti).flatMap(Date.init(timeIntervalSinceReferenceDate:)) ?? Date()
-
-      return BroadMessage(msg, date)
-    }
   }
 
 }
